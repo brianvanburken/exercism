bc53a07e839dfba65f8e06ce9daa102b66394b7d
Brian van Burken <brianvanburken@outlook.com>
2016-03-30 14:11:28 +0200

    Elixir: ETL exercise.

---
+++ elixir/etl/etl.exs
@@ -0,0 +1,17 @@
+defmodule ETL do
+  @doc """
+  Transform an index into an inverted index.
+
+  ## Examples
+
+  iex> ETL.transform(%{"a" => ["ABILITY", "AARDVARK"], "b" => ["BALLAST", "BEAUTY"]})
+  %{"ability" => "a", "aardvark" => "a", "ballast" => "b", "beauty" =>"b"}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+    for {key, val} <- input,
+        item <- val,
+        into: %{},
+        do: {String.downcase(item), key}
+  end
+end

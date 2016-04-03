98d3b475d012e69748f26021afc9632d607a12c2
Brian van Burken <brianvanburken@outlook.com>
2016-04-03 14:28:46 +0200

    Elixir: bracket-push exercise.

---
+++ elixir/bracket-push/bracket_push.exs
@@ -0,0 +1,23 @@
+defmodule BracketPush do
+  @doc """
+  Checks that all the brackets and braces in the string are matched correctly, and nested correctly
+  """
+  @spec check_brackets(String.t) :: boolean
+  def check_brackets(str) do
+    str
+    |> String.replace(~r/[^\{\}\[\]\(\)]/, "")
+    |> String.codepoints
+    |> valid?([])
+  end
+
+  defp valid?([], []), do: true
+  defp valid?([],  _), do: false
+  defp valid?([ "{" | tail ], acc), do: valid?(tail, [ "}" | acc ])
+  defp valid?([ "[" | tail ], acc), do: valid?(tail, [ "]" | acc ])
+  defp valid?([ "(" | tail ], acc), do: valid?(tail, [ ")" | acc ])
+
+  defp valid?([ "}" | tail ], [ "}" | rest ]), do: valid?(tail, rest)
+  defp valid?([ ")" | tail ], [ ")" | rest ]), do: valid?(tail, rest)
+  defp valid?([ "]" | tail ], [ "]" | rest ]), do: valid?(tail, rest)
+  defp valid?(_, _), do: false
+end

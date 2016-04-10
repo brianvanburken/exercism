71a1cd81cd585ee8f4aeb6af227c94860d17d670
Brian van Burken <brianvanburken@outlook.com>
2016-04-10 20:28:19 +0200

    Add functions on compile from mapping.

--- elixir/bracket-push/bracket_push.exs
+++ elixir/bracket-push/bracket_push.exs
@@ -1,4 +1,6 @@
 defmodule BracketPush do
+  @brackets [{"{", "}"}, {"[", "]"}, {"(", ")"}]
+
   @doc """
   Checks that all the brackets and braces in the string are matched correctly, and nested correctly
   """
@@ -12,9 +14,9 @@ defmodule BracketPush do
 
   defp valid?([], []), do: true
   defp valid?([],  _), do: false
-  defp valid?([ "{" | tail ], acc), do: valid?(tail, [ "}" | acc ])
-  defp valid?([ "[" | tail ], acc), do: valid?(tail, [ "]" | acc ])
-  defp valid?([ "(" | tail ], acc), do: valid?(tail, [ ")" | acc ])
+  for {open, close} <- @brackets do
+    defp valid?([unquote(open) | tail], acc), do: valid?(tail, [unquote(close) | acc])
+  end
   defp valid?([ x | tail ], [ x | rest ]), do: valid?(tail, rest)
   defp valid?(_, _), do: false
 end

91fd162669f46311d0dadfa4af2ab9788cf45585
Brian van Burken <brianvanburken@outlook.com>
2016-04-11 11:58:07 +0200

    Calculate on compile time instead of runtime.

--- elixir/grains/grains.exs
+++ elixir/grains/grains.exs
@@ -3,17 +3,22 @@ defmodule Grains do
   Calculate two to the power of the input minus one.
   """
   @spec square(pos_integer) :: pos_integer
-  def square(n), do: do_square(2, n - 1)
-
-  defp do_square(_, 0),        do: 1
-  defp do_square(n, 1),        do: n
-  defp do_square(n, exponent), do: n * do_square(n, exponent-1)
+  # This calculates the values for each square on compile time. This reduces the
+  # fetching of a value only to a function lookup. This improves runtime
+  # performance (which happens multiple times) in favor of decreased compile
+  # time (which is done only once). The calculations are returned and summed up
+  # from which the total method is generated. This is done so the total function
+  # just returns the total and doesn't have to calculate the total on runtime.
+  # With all this the bonus points are achieved!
+  sum = for n <- 1..64 do
+    calculation = :math.pow(2, n - 1) |> round
+    def square(unquote(n)), do: unquote(calculation)
+    calculation
+  end |> Enum.sum
 
   @doc """
   Adds square of each number from 1 to 64.
   """
   @spec total :: pos_integer
-  def total do
-    1..64 |> Enum.reduce(0, &(&2 + square(&1)))
-  end
+  def total, do: unquote(sum)
 end

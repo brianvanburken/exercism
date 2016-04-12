12693eeb927830238f29b58433522d28f668eb3e
Brian van Burken <brianvanburken@outlook.com>
2016-04-12 12:26:51 +0200

    Improve Elixir bob exercise by removing cond.

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,22 +1,20 @@
 defmodule Teenager do
   def hey(input) do
-    stripped  = String.strip(input)
-    cond do
-      silent?(stripped) -> "Fine. Be that way!"
-      yelling?(stripped) -> "Whoa, chill out!"
-      question?(stripped) -> "Sure."
-      true -> "Whatever."
-    end
+    respond(
+      String.strip(input) === "",
+      String.upcase(input) === input && String.downcase(input) !== input,
+      String.ends_with?(input, "?")
+    )
   end
 
-  defp silent?(""), do: true
-  defp silent?(_), do: false
-
-  defp yelling?(input) do
-    String.upcase(input) === input && String.downcase(input) !== input
-  end
-
-  defp question?(input)  do
-    String.ends_with?(input, "?")
-  end
+  # This matches based on the conditions. I've chosen to not be like the rest of
+  # the solutions and limited myself to not use cond or case. I did some
+  # benchmarks and found out that this pattern matching is more performant. The
+  # performance difference is nihil. Below the numbers of the benchmark:
+  # parameters 1.27 µs/op
+  # cond       1.32 µs/op
+  defp respond(true, _, _), do: "Fine. Be that way!"
+  defp respond(_, true, _), do: "Whoa, chill out!"
+  defp respond(_, _, true), do: "Sure."
+  defp respond(_, _, _),    do: "Whatever."
 end

5fcfd8f97cc8bbc4e44357973a87029185a752e7
Brian van Burken <brianvanburken@outlook.com>
2016-04-15 10:48:16 +0200

    Generate functions on compile time for space age.

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -1,19 +1,25 @@
 defmodule SpaceAge do
-  @year_in_seconds_on_earth 31_557_600
   @type planet :: :mercury | :venus | :earth | :mars | :jupiter
                 | :saturn | :neptune | :uranus
+
+  @year_in_seconds_on_earth 31_557_600
+  @ratios [
+    { :earth,  1.00000000 }, { :mercury, 0.24084670 }, { :venus,  0.61519726 },
+    { :mars,   1.88081580 }, { :jupiter, 11.8626150 }, { :saturn, 29.447498  },
+    { :uranus, 84.016846  }, { :neptune, 164.79132  }
+  ]
 
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
-  def age_on(:earth,   seconds), do: (seconds / @year_in_seconds_on_earth) / 1.00000000
-  def age_on(:mercury, seconds), do: (seconds / @year_in_seconds_on_earth) / 0.24084670
-  def age_on(:venus,   seconds), do: (seconds / @year_in_seconds_on_earth) / 0.61519726
-  def age_on(:mars,    seconds), do: (seconds / @year_in_seconds_on_earth) / 1.88081580
-  def age_on(:jupiter, seconds), do: (seconds / @year_in_seconds_on_earth) / 11.8626150
-  def age_on(:saturn,  seconds), do: (seconds / @year_in_seconds_on_earth) / 29.447498
-  def age_on(:uranus,  seconds), do: (seconds / @year_in_seconds_on_earth) / 84.016846
-  def age_on(:neptune, seconds), do: (seconds / @year_in_seconds_on_earth) / 164.79132
+  # This generates for each ratio a function that matches on the planet. This is
+  # done on compile time. To add more planets it's a easy as adding a new ratio
+  # tuple to the @ratios module attribute and compile.
+  for { planet, ratio } <- @ratios do
+    def age_on(unquote(planet), seconds) do
+      ( seconds / unquote(@year_in_seconds_on_earth) ) / unquote(ratio)
+    end
+  end
 end

63e9852f086a9b40ed8a614a676ad07e6e48be50
Brian van Burken <brianvanburken@outlook.com>
2016-04-15 17:05:34 +0200

    More performant version of Elixir abbreviate.
    This eliminates the recurse done by List.flatten. Instead it now just
    takes the first element and joins it directly.

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -6,8 +6,7 @@ defmodule Acronym do
   @spec abbreviate(string) :: String.t()
   def abbreviate(string) do
     Regex.scan(~r{([A-Za-z])[a-z]+}, string, capture: :all_but_first)
-    |> List.flatten
-    |> Enum.join
+    |> Enum.map_join(&List.first/1)
     |> String.upcase
   end
 end

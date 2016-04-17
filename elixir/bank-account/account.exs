322ec25d6ab7bedf48f10c1bbc5b186be299d0bf
Brian van Burken <brianvanburken@outlook.com>
2016-04-17 19:40:45 +0200

    Elixir: bank-account exercise.

---
+++ elixir/bank-account/account.exs
@@ -0,0 +1,43 @@
+defmodule BankAccount do
+  @moduledoc """
+  A bank account that supports access from multiple processes.
+  """
+
+  @typedoc """
+  An account handle.
+  """
+  @opaque account :: pid
+
+  @doc """
+  Open the bank. Makes the account available.
+  """
+  @spec open_bank() :: account
+  def open_bank() do
+    {:ok, account} = Agent.start(fn -> 0 end)
+    account
+  end
+
+  @doc """
+  Close the bank. Makes the account unavailable.
+  """
+  @spec close_bank(account) :: none
+  def close_bank(account) do
+    Agent.stop(account) 
+  end
+
+  @doc """
+  Get the account's balance.
+  """
+  @spec balance(account) :: integer
+  def balance(account) do
+    Agent.get(account, &(&1))
+  end
+ 
+  @doc """
+  Update the account's balance by adding the given amount which may be negative.
+  """
+  @spec update(account, integer) :: any
+  def update(account, amount) do
+    Agent.update(account, &(&1 + amount))
+  end
+end

dfc750c34314b91c6ecd45ceff1e5bd1d7150311
Brian van Burken <brianvanburken@outlook.com>
2016-04-18 21:26:26 +0200

    Improve RNA transcribe for Elixir.

--- elixir/rna-transcription/dna.exs
+++ elixir/rna-transcription/dna.exs
@@ -1,10 +1,4 @@
 defmodule DNA do
-  @nucleotides %{
-    ?G => ?C,
-    ?C => ?G,
-    ?T => ?A,
-    ?A => ?U
-  }
   @doc """
   Transcribes a character list representing DNA nucleotides to RNA
 
@@ -14,7 +8,11 @@ defmodule DNA do
   'UGAC'
   """
   @spec to_rna([char]) :: [char]
-  def to_rna(dna) do
-    dna |> Enum.map(&Map.get(@nucleotides, &1))
+  def to_rna(dna, acc \\ [])
+  def to_rna([], acc), do: acc |> Enum.reverse
+  for { dna, rna } <- %{ ?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U } do
+    def to_rna([ unquote(dna) | tail ], acc) do
+      to_rna(tail, [ unquote(rna) | acc ])
+    end
   end
 end

6e648139a460c8a82e887a619911e5f63fee6620
Brian van Burken <brianvanburken@outlook.com>
2016-04-19 11:10:22 +0200

    Walk the binary string.

--- elixir/bracket-push/bracket_push.exs
+++ elixir/bracket-push/bracket_push.exs
@@ -1,5 +1,5 @@
 defmodule BracketPush do
-  @brackets [{?{, ?}}, {?[, ?]}, {?(, ?)}]
+  @brackets [{"{", "}"}, {"[", "]"}, {"(", ")"}]
 
   @doc """
   Checks that all the brackets and braces in the string are matched correctly,
@@ -11,24 +11,24 @@ defmodule BracketPush do
   def check_brackets(str) do
     str
     |> String.replace(~r/[^\{\}\[\]\(\)]/, "")
-    |> to_char_list
-    |> valid?([])
+    |> valid?
   end
 
   # This function pattern matches on each opening/closing tag by using tail
   # recursion. For each opening bracket its corresponding closing bracket is
   # pushed to the accumulator. When a closing bracket is encounter it gets matched
-  # with the head of the accumulator. If this matches it recurses further. If an
+  # with the head of the accumulator. If this matches, it recurses further. If an
   # incorrect bracket is encountered it doesn't match and ends up with the last
   # match that returns false. If there is an unclosed bracket it ends up matching
   # the second function and returns false. If in the end all is well we end up
-  # with and empty codepoints list and empty accumulator list. For this we return
+  # with and empty binary stringt and empty accumulator list. For this we return
   # true.
-  defp valid?([], []), do: true
-  defp valid?([],  _), do: false
+  defp valid?(string, accumulator \\ [])
+  defp valid?("", []), do: true
+  defp valid?("",  _), do: false
   for {open, close} <- @brackets do
-    defp valid?([unquote(open) | tail], acc), do: valid?(tail, [unquote(close) | acc])
+    defp valid?(unquote(open) <> tail, acc), do: valid?(tail, [unquote(close) | acc])
   end
-  defp valid?([ x | tail ], [ x | rest ]), do: valid?(tail, rest)
+  defp valid?(<<x::binary-size(1), tail::binary>>, [ x | rest ]), do: valid?(tail, rest)
   defp valid?(_, _), do: false
 end

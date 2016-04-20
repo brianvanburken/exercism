d0a404d5d66781fc1fa98ae6d72edce01b5b7350
Brian van Burken <brianvanburken@outlook.com>
2016-04-20 11:19:35 +0200

    Elixir: zipper exercise.

---
+++ elixir/zipper/zipper.exs
@@ -0,0 +1,105 @@
+defmodule BinTree do
+  @moduledoc """
+  A node in a binary tree.
+
+  `value` is the value of a node.
+  `left` is the left subtree (nil if no subtree).
+  `right` is the right subtree (nil if no subtree).
+  """
+  @type t :: %BinTree{ value: any, left: BinTree.t | nil, right: BinTree.t | nil }
+  defstruct value: nil, left: nil, right: nil
+end
+
+defmodule BinTree.Zipper do
+  @moduledoc """
+  A binary tree zipper.
+  `value` is the value of the focus.
+  `left` is the left subtree of the focus.
+  `right` is the right subtree of the focus.
+  `trail` is the trail of the focus.
+  The trail stores the path that a zipper has taken into the tree from the root
+  and the alternative branches.
+  """
+  @type t :: %BinTree.Zipper{
+      value: any,
+      left: BinTree.Zipper.t | nil,
+      right: BinTree.Zipper.t | nil,
+      trail: [ { :left, any, BinTree.t } | { :right, any, BinTree.t } ]
+    }
+  defstruct value: nil, left: nil, right: nil, trail: []
+end
+
+defmodule Zipper do
+  alias BinTree, as: BT
+  alias BinTree.Zipper, as: Z
+
+  @doc """
+
+  Get a zipper focused on the root node.
+  """
+  @spec from_tree(BT.t) :: Z.t
+  def from_tree(%BT{ value: v, left: l, right: r}), do: %Z{value: v, left: l, right: r, trail: []}
+
+  @doc """
+  Get the complete tree from a zipper.
+  """
+  @spec to_tree(Z.t) :: BT.t
+  def to_tree(%Z{value: v, left: l, right: r, trail: t}), do: _to_tree(%BT{value: v, left: l, right: r}, t)
+  defp _to_tree(b, []), do: b
+  defp _to_tree(b, [{ :left, pv, pr } | pt]), do: do_to_tree(%BT{value: pv, left: b, right: pr}, pt)
+  defp _to_tree(b, [{ :right, pv, pl } | pt]), do: do_to_tree(%BT{value: pv, left: pl, right: b}, pt)
+
+  @doc """
+  Get the value of the focus node.
+  """
+  @spec value(Z.t) :: any
+  def value(%Z{ value: v }), do: v
+
+  @doc """
+  Get the left child of the focus node, if any.
+  """
+  @spec left(Z.t) :: Z.t | nil
+  def left(%Z{ left: nil }), do: nil
+  def left(%Z{ value: v, left: %BT{value: lv, left: ll, right: lr}, right: r, trail: zt}) do
+    %Z{ value: lv, left: ll, right: lr, trail: [{ :left, v, r } | zt] }
+  end
+
+  @doc """
+  Get the right child of the focus node, if any.
+  """
+  @spec right(Z.t) :: Z.t | nil
+  def right(%Z{ right: nil }), do: nil
+  def right(%Z{ value: v, left: l, right: %BT{value: rv, left: rl, right: rr}, trail: zt }) do
+    %Z{ value: rv, left: rl, right: rr, trail: [{ :right, v, l } | zt] }
+  end
+
+  @doc """
+  Get the parent of the focus node, if any.
+  """
+  @spec up(Z.t) :: Z.t
+  def up(%Z{ trail: [] }), do: nil
+  def up(%Z{ value: v, left: l, right: r, trail: [{ :left, pv, pr } | zt] }) do
+    %Z{ value: pv, left: %BT{value: v, left: l, right: r}, right: pr, trail: zt }
+  end
+  def up(%Z{ value: v, left: l, right: r, trail: [{ :right, pv, pl } | zt] }) do
+    %Z{ value: pv, left: pl, right: %BT{value: v, left: l, right: r}, trail: zt }
+  end
+
+  @doc """
+  Set the value of the focus node.
+  """
+  @spec set_value(Z.t, any) :: Z.t
+  def set_value(z, v), do: %{z | value: v}
+
+  @doc """
+  Replace the left child tree of the focus node.
+  """
+  @spec set_left(Z.t, BT.t) :: Z.t
+  def set_left(z, l), do: %{z | left: l}
+
+  @doc """
+  Replace the right child tree of the focus node.
+  """
+  @spec set_right(Z.t, BT.t) :: Z.t
+  def set_right(z, r), do: %{z | right: r}
+end

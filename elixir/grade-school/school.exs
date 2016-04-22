33d064e8c1f69009f514165faa0797456c60229f
Brian van Burken <brianvanburken@outlook.com>
2016-04-22 09:00:30 +0200

    Place single line code in do: block.

--- elixir/grade-school/school.exs
+++ elixir/grade-school/school.exs
@@ -9,23 +9,17 @@ defmodule School do
   Add a student to a particular grade in school.
   """
   @spec add(map, String.t, integer) :: map
-  def add(db, name, grade) do
-    Map.update(db, grade, [name], &([ name | &1 ]))
-  end
+  def add(db, name, grade), do: Map.update(db, grade, [name], &([ name | &1 ]))
 
   @doc """
   Return the names of the students in a particular grade.
   """
   @spec grade(map, integer) :: [String.t]
-  def grade(db, grade) do
-    Map.get(db, grade, [])
-  end
+  def grade(db, grade), do: Map.get(db, grade, [])
 
   @doc """
   Sorts the school by grade and name.
   """
   @spec sort(map) :: [{integer, [String.t]}]
-  def sort(db) do
-    for { grade, students } <- db, do: { grade, Enum.sort(students) }
-  end
+  def sort(db), do: for { grade, students } <- db, do: { grade, Enum.sort(students) }
 end

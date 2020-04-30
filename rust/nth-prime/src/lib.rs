deb50b5564c2391221f4d00261e847e72ef8b1b5
Brian van Burken <brianvanburken@gmail.com>
2020-04-30 17:14:31 +0200

    Rust: nth-prime

---
+++ rust/nth-prime/src/lib.rs
@@ -0,0 +1,33 @@
+pub fn nth(n: u32) -> u32 {
+    let mut number = 0;
+    let mut found_primes = 0;
+    loop {
+        number += 1;
+        let prime = is_prime(number);
+        if prime {
+            found_primes += 1;
+            if (found_primes - 1) == n {
+                break;
+            }
+        }
+    }
+    number
+}
+
+fn is_prime(n: u32) -> bool {
+    if n == 1 {
+        return false;
+    }
+    if n == 2 {
+        return true;
+    }
+    if n % 2 == 0 {
+        return false;
+    }
+    for a in 3..n {
+        if n % a == 0 {
+            return false;
+        }
+    }
+    true
+}

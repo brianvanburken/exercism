ded2ab591384264ae022975bda4e0f1af8397d39
Brian van Burken <brianvanburken@gmail.com>
2020-08-26 14:43:43 +0200

    Kotlin: DnD character

---
+++ kotlin/dnd-character/src/main/kotlin/DndCharacter.kt
@@ -0,0 +1,24 @@
+import kotlin.random.Random
+
+class DndCharacter {
+
+    val strength: Int = ability()
+    val dexterity: Int = ability()
+    val constitution: Int = ability()
+    val intelligence: Int = ability()
+    val wisdom: Int = ability()
+    val charisma: Int = ability()
+    val hitpoints: Int = 10 + modifier(constitution)
+
+    companion object {
+
+        fun ability(): Int = (1..4)
+            .map { Random.nextInt(1, 6) }
+            .sorted()
+            .drop(1)
+            .sum()
+
+        fun modifier(score: Int): Int = score / 2 - 5
+    }
+
+}

a301a4cc5ee4864eeb9a8748136c4f7ea766ac68
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-07-19 12:21:14 +0200

    Rust: resistor color

---
+++ rust/resistor-color/src/lib.rs
@@ -0,0 +1,31 @@
+use enum_iterator::{all, Sequence};
+use int_enum::IntEnum;
+
+#[repr(usize)]
+#[derive(Debug, Copy, Clone, PartialEq, IntEnum, Sequence)]
+pub enum ResistorColor {
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
+}
+
+pub fn color_to_value(color: ResistorColor) -> usize {
+    color.int_value()
+}
+
+pub fn value_to_color_string(value: usize) -> String {
+    ResistorColor::from_int(value)
+        .map(|c| format!("{:?}", c))
+        .unwrap_or_else(|_| "value out of range".into())
+}
+
+pub fn colors() -> Vec<ResistorColor> {
+    all::<ResistorColor>().collect()
+}

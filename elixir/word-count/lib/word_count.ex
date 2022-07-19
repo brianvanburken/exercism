7a4b2359b2192275c574ae7c0552932ae07b74df
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-07-19 14:26:47 +0200

    fix: upgrade Elixir word-count

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -1,14 +1,15 @@
-defmodule Words do
+defmodule WordCount do
   @doc """
   Count the number of words in the sentence.
 
   Words are compared case-insensitively.
   """
-  @spec count(String.t) :: map()
+  @spec count(String.t()) :: map()
   def count(sentence) do
     sentence
-    |> String.downcase
-    |> String.split(~r/[!@%$^&* _,:]+/, trim: true)
+    |> String.downcase()
+    |> String.split(~r/[!@%$^&* _,:\n.]+/, trim: true)
+    |> Enum.map(&String.trim(&1, "'"))
     |> Enum.reduce(%{}, &count_words/2)
   end
 

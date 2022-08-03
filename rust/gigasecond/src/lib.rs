c5dd9d0f44e3dc72d740ac6c062ad1d6bb69ba23
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-08-03 10:16:45 +0200

    Rust: gigasecond

---
+++ rust/gigasecond/src/lib.rs
@@ -0,0 +1,6 @@
+use time::{Duration, PrimitiveDateTime as DateTime};
+
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
+    start + Duration::seconds(1_000_000_000)
+}

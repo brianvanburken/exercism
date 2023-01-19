7bc4eb26fc33bdaeb474a62cec5179262adf4a7c
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-01-19 20:00:41 +0100

    performance: use the bit field approach as an example

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -2,11 +2,11 @@ use std::collections::HashSet;
 
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    let mut alphabet = [0; 26];
+    let mut alphabet: u32 = 0;
     for c in sentence.to_lowercase().chars() {
         if c >= 'a' && c <= 'z' {
-            alphabet[(c as u8 - 'a' as u8) as usize] += 1;
+            alphabet |= 1 << (c as u8 - 'a' as u8) as usize;
         }
     }
-    !alphabet.iter().any(|&x| x == 0)
+    alphabet == (1 << 26) - 1
 }

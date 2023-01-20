0932ab334f8d6773e60290ad598b75eeda28e45a
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-01-20 11:26:30 +0100

    perf: improve space complexity for Rust pangram
    Instead of converting to lowercase we could also check for the range in
    uppercase. This reduces the space complexity by not having to convert
    the string to lowercase.

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,10 +1,10 @@
-use std::collections::HashSet;
-
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
     let mut alphabet: u32 = 0;
-    for c in sentence.to_lowercase().chars() {
-        if c >= 'a' && c <= 'z' {
+    for c in sentence.chars() {
+        if c >= 'A' && c <= 'Z' {
+            alphabet |= 1 << (c as u8 - 'A' as u8) as usize;
+        } else if c >= 'a' && c <= 'z' {
             alphabet |= 1 << (c as u8 - 'a' as u8) as usize;
         }
     }

9b755c79921f160488ae3b8eab5ea493cfd41664
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-01-22 14:23:47 +0100

    feat: Rust high scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        Self { scores }
+    }
+
+    pub fn scores(&self) -> &'a [u32] {
+        &self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().cloned()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut scores = self.scores.to_vec();
+        scores.sort_by(|a, b| b.cmp(a));
+        scores.truncate(3);
+        scores
+    }
+}

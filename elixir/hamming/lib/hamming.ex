a5d6e9d39ffc588d568794fc39001987871758ae
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-02-18 22:28:45 +0100

    refactor: improve public interface of Elixir hamming exercise

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -7,6 +7,9 @@ defmodule Hamming do
   iex> Hamming.hamming_distance('AAGTCATA', 'TAGCGATC')
   {:ok, 4}
   """
+  @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
+  def hamming_distance(strand1, strand2), do: calculate(strand1, strand2, 0)
+
   # This walks through both strands at the same time; character by character. The
   # first function will match the strands on the same character. In this case it
   # continues using the tail. If there is a difference in the characters on the
@@ -14,13 +17,13 @@ defmodule Hamming do
   # length it will match the third function and return the accumulator. If one of
   # the strands differ from the other it ends up matching the last function which
   # returns nil.
-  @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
-  def hamming_distance(strand1, strand2, acc \\ 0)
-  def hamming_distance(l1, l2, _) when length(l1) != length(l2), do: {:error, "strands must be of equal length"}
-  def hamming_distance([ ha | ta ], [ ha | tb ], acc), do: hamming_distance(ta, tb, acc)
-  def hamming_distance([ _  | ta ], [ _  | tb ], acc), do: hamming_distance(ta, tb, acc + 1)
-  def hamming_distance([], [], acc), do: {:ok, acc}
-  def hamming_distance('', _, 0), do: {:error, "strands must be of equal length"}
-  def hamming_distance(_, '', 0), do: {:error, "strands must be of equal length"}
-  def hamming_distance(_,  _, _), do: nil
+  defp calculate(strand1, strand2, _) when length(strand1) != length(strand2),
+    do: {:error, "strands must be of equal length"}
+
+  defp calculate([ha | ta], [ha | tb], acc), do: calculate(ta, tb, acc)
+  defp calculate([_ | ta], [_ | tb], acc), do: calculate(ta, tb, acc + 1)
+  defp calculate([], [], acc), do: {:ok, acc}
+  defp calculate('', _, 0), do: {:error, "strands must be of equal length"}
+  defp calculate(_, '', 0), do: {:error, "strands must be of equal length"}
+  defp calculate(_, _, _), do: nil
 end

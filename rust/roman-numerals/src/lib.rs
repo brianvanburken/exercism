afa2bc90a9506a69f5e9d17d452fcd495370aefb
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-02-28 22:17:19 +0100

    perf: improve roman numerals performance

--- rust/roman-numerals/src/lib.rs
+++ rust/roman-numerals/src/lib.rs
@@ -1,35 +1,34 @@
-use std::{fmt::{Display, Formatter, Result}, collections::HashMap};
+use std::fmt::{Display, Formatter, Result};
 
 static ROMAN_MAP: [(usize, &str); 13] = [
-    (1, "I"),
-    (4, "IV"),
-    (5, "V"),
-    (9, "IX"),
-    (10, "X"),
-    (40, "XL"),
-    (50, "L"),
-    (90, "XC"),
-    (100, "C"),
-    (400, "CD"),
-    (500, "D"),
-    (900, "CM"),
     (1000, "M"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I"),
 ];
-
 
 pub struct Roman(usize);
 
 impl Display for Roman {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         let mut start = self.0;
-        let lookup = HashMap::from(ROMAN_MAP);
         let mut result = String::new();
-        while start > 0 {
-            let max_val = *lookup.keys().filter(|&x| x <= &start).max().unwrap();
-            result.push_str(lookup[&max_val]);
-            start -= max_val;
+        for &(value, symbol) in ROMAN_MAP.iter() {
+            while start >= value {
+                result.push_str(symbol);
+                start -= value;
+            }
         }
-        write!(f, "{}", result)
+        write!(f, "{result}")
     }
 }
 

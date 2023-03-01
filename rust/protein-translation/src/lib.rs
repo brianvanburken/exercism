8f673066fedea8f84e444c14e17c391ba099a5c1
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-03-01 12:39:05 +0100

    feat: Rust Protein Translation

---
+++ rust/protein-translation/src/lib.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+pub struct CodonsInfo<'a> {
+    mapping: HashMap<&'a str, &'a str>,
+}
+
+impl<'a> CodonsInfo<'a> {
+    pub fn name_for(&self, codon: &str) -> Option<&'a str> {
+        self.mapping.get(&codon).cloned()
+    }
+
+    pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
+        rna.chars()
+            .collect::<Vec<char>>()
+            .chunks(3)
+            .map(|chars| self.name_for(&chars.iter().collect::<String>()))
+            .take_while(|result| result.is_none() || result != &Some("stop codon"))
+            .collect()
+    }
+}
+
+pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
+    CodonsInfo {
+        mapping: HashMap::from_iter(pairs.into_iter()),
+    }
+}
